db7b47b01c9ed9ed07ba507be3c544228fe3d292
Valeron01 <electronpochta3011@mail.ru>
2021-10-05 20:52:21 +0300

    added index auth handler

--- main/views.py
+++ main/views.py
@@ -2,17 +2,33 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, JsonResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.template.loader import render_to_string
+
+from .models import *
 
 
 def index(request:HttpRequest):
     if request.method == 'GET':
         return render(request, 'index.html')
 
+    if request.method == 'POST':
+        if request.user.is_authenticated:
+            return render_to_string('_profile.html')
+        else:
+            return render_to_string('_auth.html')
+
 def register(request:HttpRequest):
     if request.method == 'GET':
         return render(request, '_register.html')
     if request.method == 'POST':
-        new_user = User.objects.create(username=request.POST['login'], email=request.POST['login'], password=request.POST['password'])
+        new_user = User.objects.create(username=request.POST['email'], email=request.POST['email'], password=request.POST['password'])
+
+        user_detail = UserDetail.objects.create(
+            new_user,
+            request.POST['firstName'], request.POST['lastName'],
+            request.POST['patronymic'], request.POST['isTeacher'], 
+            request.POST['studentCode'])
+        
         new_user.save()
         login(request, new_user)
 
@@ -24,7 +40,7 @@ def auth(request:HttpRequest):
     if request.method == 'GET':
         return render(request, '_auth.html')
     if request.method == 'POST':
-        user = authenticate(username=request.POST['login'], password=request.POST['password'])
+        user = authenticate(email=request.POST['email'], password=request.POST['password'])
 
         if user is not None:
             login(request, user)
