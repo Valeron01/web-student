0c24473a2917250de199bf32130d6c9b0a562d06
agrowein <geo.harri40@gmail.com.com>
2021-10-05 20:21:20 +0300

    Added table builder and panel builder functions

--- main/static/js/script.js
+++ main/static/js/script.js
@@ -5,7 +5,9 @@ window.onload = function() {
 // Инициализация элементов 
 // Проверка авторизации
 async function init() {
-    await changePage('/auth');
+    await changePage('/profile');
+    buildTable([{name: "Мат.Анализ", teacher: "Сирота Е.А.", mark: 3}]);
+    buildPanel([{id: 1, name: "I семестр"}]);
 }
 
 // Изменение стандарного поведения ссылок 
@@ -17,6 +19,52 @@ function bindQueriesForLinks() {
         changePage(e.target.getAttribute('href'));
     })
 };
+
+//Функция заполнения таблицы с предметами
+//data -> array of objects {name, teacher, mark}
+function buildTable(data) {
+    const table = $('#table');
+    let header = `<tr class="table__header"><th>Название</th><th>Преподаватель</th><th>Оценка</th></tr>`;
+    let inner = data.reduce((acc, el) => {
+         return acc + `<tr>
+                    <td>${el.name}</td>
+                    <td>${el.teacher}</td>
+                    <td>${el.mark}</td>
+                </tr>`;
+    }, header);
+    
+    table.html(inner);
+}
+
+// Функция заполения панели кнопками выбора семестра
+// data -> array of objects {id, name}
+function buildPanel(data) {
+    let ids = []
+    let inner = data.reduce((acc, el) => {
+        ids.push({id: 'btn'+el.name, num: el.id});
+        return acc + `<button id="${'btn' + el.name}"class="button semesterBtn">${el.name}</button>`
+    }, '');
+    $("#subjects__list").html(inner);
+
+    ids.forEach((el) => {
+        $('#'+el.id).on('click', () => semesterClickHandler(el.num));
+    });
+    console.log(inner);
+}
+
+// Фунция обработчик клика по кнопке в панели семстров 
+// Запрос, ответ, заполнение таблицы данными
+function semesterClickHandler(id) {
+    $.ajax({
+        url: '/semester',
+        method: "POST",
+        dataType: "json",
+        data: {
+            id
+        }
+    })
+    .done(data => buildTable(data));
+}
 
 // Изменить содержимое блока root
 async function changePage(url) {
@@ -49,3 +97,4 @@ function checkboxHandler(event) {
         $("#student-number").prop("disabled", false);
     }
 }
+
