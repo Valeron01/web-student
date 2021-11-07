3b9a65abdac2f0414aee16c9c550677b9a401627
Valeron01 <electronpochta3011@mail.ru>
2021-11-07 21:11:05 +0300

    fixed manu gugs, fuck db.sqlite3

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Teacher)
-admin.site.register(Student)
 admin.site.register(Mark)
 admin.site.register(Subject)
 admin.site.register(UserDetail)
