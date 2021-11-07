93e4b9d06e8d80023d6b1dc66632add455ea1551
Valeron01 <electronpochta3011@mail.ru>
2021-11-07 21:54:56 +0300

    bugfix, go 7dtd

--- main/admin.py
+++ main/admin.py
@@ -2,8 +2,17 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Mark)
-admin.site.register(Subject)
 admin.site.register(UserDetail)
 admin.site.register(Semester)
 
 # Register your models here.
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    def response_add(self, request, obj):
+        students = UserDetail.objects.filter(is_teacher=False)
+
+        for stud in students:
+            new_mark = Mark.objects.create(user=stud.user, subject=obj, mark=-1)
+        
+        return super().response_change(request, obj)
