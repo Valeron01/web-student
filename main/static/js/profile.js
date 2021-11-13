8fc796c7ea5d9260a4d92421f6573f8a14456689
Artem Chebotarev <geo.harri40@gmai.com>
2021-11-13 14:45:23 +0300

    Added profile for teacher

--- main/static/js/profile.js
+++ main/static/js/profile.js
@@ -5,9 +5,8 @@ window.onload = function() {
         dataType: "json"
     })
     .done((res) => {
-
         initProfile(res.info)
-        buildPanel(res.semester)
+        res.info.is_teacher ? buildSubjectPanel(res.subject) : buildSemesterPanel(res.semester)
         console.log(res.semester);
     })
 }
@@ -16,6 +15,38 @@ function initProfile(data) {
     $('#role').text(data.role)
     $('#name').text(data.name)
     $('#email').text(data.email)
+}
+
+function buildSubjectPanel(data) {
+    let ids = []
+    let inner = data.reduce((acc, el) => {
+        ids.push({id: 'btn'+el.id, num: el.id});
+        return acc + `<button id="${'btnSub' + el.id}"class="button subjectBtn">${el.name}</button>`
+    }, '');
+    $("#subjects__list").html(inner);
+    
+
+    ids.forEach((el) => {
+        $('#'+el.id).on('click', () => {
+            subjectClickHandler(el.num)
+        });
+    });
+}
+
+function buildSubjectTable(data) {
+    const table = $('#table');
+    let header = `<tr class="table__header"><th>Студент</th><th>Оценка</th></tr>`;
+    let inner = data.marks.reduce((acc, el) => {
+         return acc + `<tr>
+                    <td>${el.name}</td>
+                    <td>
+                        <input class="inputMark" type="text" value="${el.mark}">
+                    </td>
+                </tr>`;
+    }, header);
+    inner += '<button id="btnSave">Сохранить</button>'
+    table.html(inner);
+    $("#btnSave").on('click', sendMarkData)
 }
 
 //Функция заполнения таблицы с предметами
@@ -37,7 +68,6 @@ function buildTable(data) {
 // Фунция обработчик клика по кнопке в панели семстров 
 // Запрос, ответ, заполнение таблицы данными
 const semesterClickHandler = (id) => {
-    console.log("ddede");
     $.ajax({
         url: '/get_panel_data',
         method: "POST",
@@ -49,15 +79,32 @@ const semesterClickHandler = (id) => {
     .done(data => buildTable(data));
 }
 
+const subjectClickHandler = (id) => {
+    $.ajax({
+        url: '/get_subject_data',
+        method: "POST",
+        dataType: "json",
+        data: {
+            id
+        }
+    })
+    .done(data => buildSubjectTable(data));
+}
+
+const sendMarkData = () => {
+
+} 
+
 // Функция заполения панели кнопками выбора семестра
 // data -> array of objects {id, name}
-function buildPanel(data) {
+function buildSemesterPanel(data) {
     let ids = []
     let inner = data.reduce((acc, el) => {
         ids.push({id: 'btn'+el.id, num: el.id});
         return acc + `<button id="${'btn' + el.id}"class="button semesterBtn">${el.name}</button>`
     }, '');
     $("#subjects__list").html(inner);
+    
 
     ids.forEach((el) => {
         $('#'+el.id).on('click', () => {
